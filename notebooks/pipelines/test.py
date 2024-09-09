24f7943a77360d22d663de55c1cffb4e615755be
Gaurav M <gaurav45750@gmail.com>
2024-09-09 14:59:54 +0530

    adding changes

---
+++ notebooks/pipelines/test.py
@@ -0,0 +1,23 @@
+
+import kfp.compiler
+from kfp import dsl
+
+#
+@dsl.component(base_image="image-registry.openshift-image-registry.svc:5000/openshift/python:latest",
+               packages_to_install=["yfinance"])
+def download_data(ticker):
+    import yfinance as yfin
+    """Calculate the sum of the two arguments."""
+    #ticker = os.environ.get('TICKER')
+    df = yfin.download(tickers=ticker, period='6mo')
+    dataset = df['Close'].fillna(method='ffill')
+    dataset = dataset.values.reshape(-1, 1)
+
+@dsl.pipeline()
+def add_pipeline(ticker: str = 'AAPL'):
+    """Pipeline to add values.
+    """
+    first_add_task = download_data(ticker)
+
+if __name__ == "__main__":
+    kfp.compiler.Compiler().compile(add_pipeline, package_path=__file__.replace(".py", ".yaml"))
